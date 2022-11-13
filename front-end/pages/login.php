a808c5f921e2de9f37f8ae5fd205da83f23b2bd6
jasonwillhudson <38706702+jasonwillhudson@users.noreply.github.com>
2022-11-12 23:19:10 -0800

    init

---
+++ front-end/pages/login.php
@@ -0,0 +1,16 @@
+<?php
+require "../components/header.php";
+?>
+
+<form method="post">
+<label>Email</label>
+<input type="text" name="email"/>
+<label>Password</label>
+<input type="password" name="password"/>
+
+</form>
+
+
+<?php
+require "../components/footer.php";
+?>
