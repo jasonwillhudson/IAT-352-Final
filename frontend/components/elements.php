2562673700901a395e8d44a3a33d8da2efdc5034
AnnY0v0 <118229963+AnnY0v0@users.noreply.github.com>
2022-12-01 09:10:56 -0800

    Update elements.php

--- frontend/components/elements.php
+++ frontend/components/elements.php
@@ -14,3 +14,29 @@ function makeInput($label, $name, $type, $value = "")
     else //display the input with the type user chose
         echo "<input type=$type name=$name value=\"$value\"></input></div>";
 }
+
+//Create filters for user to choose types of goods
+function createFilter($name, $value, $var = '')
+{
+   $ischecked = false; // not checked the first element yet
+   foreach ($value as $v) {
+
+      //if no variable defined for audio button
+      if (empty($var)) {
+         if ($ischecked) echo "<input type=\"checkbox\" name=$name value=$v>";
+         else {
+            echo "<input type=\"checkbox\" name=$name value=$v checked>";
+            $ischecked = true; //checked the element already
+         }
+      } else {
+         if ($ischecked || $var != $v) echo "<input type=\"checkbox\" name=$name value=$v>"; //if not checked or values of variables not equal
+         else {
+            echo "<input type=\"checkbox\" name=$name value=$v checked>";
+            $ischecked = true; //checked the element already
+         }
+      }
+
+      echo "<label for=$v>$v</label>"; //label for the radio button
+
+   }
+}
