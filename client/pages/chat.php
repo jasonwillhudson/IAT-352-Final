1bb5a1ae62ccc722af1441333c5f5fecd044458c
AnnY0v0 <118229963+AnnY0v0@users.noreply.github.com>
2022-12-02 01:25:36 -0800

    Update chat.php

--- client/pages/chat.php
+++ client/pages/chat.php
@@ -66,7 +66,8 @@ function loginForm(){
         </div>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript">
-            // jQuery Document
+            
+         // jQuery
             $(document).ready(function () {
                 $("#submitmsg").click(function () {
                     var clientmsg = $("#usermsg").val();
