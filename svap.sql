d5934036fe2efcaf2bf38497c9fc09bc142b40b5
Chongyuan Bian <114266253+ChongyuanBian@users.noreply.github.com>
2022-12-10 03:56:16 -0800

    final database

--- svap.sql
+++ svap.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： localhost
--- 生成日期： 2022-12-08 19:20:33
+-- 生成日期： 2022-12-10 12:55:09
 -- 服务器版本： 10.4.21-MariaDB
 -- PHP 版本： 8.0.10
 
@@ -24,18 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 表的结构 `admin`
---
-
-CREATE TABLE `admin` (
-  `admin_id` int(10) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- 表的结构 `collection`
 --
 
@@ -43,22 +31,6 @@ CREATE TABLE `collection` (
   `collector_email` varchar(50) NOT NULL,
   `post_id` int(20) NOT NULL,
   `collection_id` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `comment`
---
-
-CREATE TABLE `comment` (
-  `question` varchar(100) NOT NULL,
-  `member_id` int(20) NOT NULL,
-  `post_id` int(20) NOT NULL,
-  `question_time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `comment_id` int(11) NOT NULL,
-  `answer` varchar(100) NOT NULL,
-  `answer_time` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -72,17 +44,6 @@ CREATE TABLE `image_path` (
   `post_id` int(20) NOT NULL,
   `image_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- 转存表中的数据 `image_path`
---
-
-INSERT INTO `image_path` (`email`, `post_id`, `image_path`) VALUES
-('1762649548@qq.com', 1, 'imgStorage/1762649548@qq.com/1/0.png'),
-('1762649548@qq.com', 1, 'imgStorage/1762649548@qq.com/1/1.png'),
-('1762649548@qq.com', 2, 'imgStorage/1762649548@qq.com/2/0.png'),
-('1762649548@qq.com', 3, 'imgStorage/1762649548@qq.com/3/0.png'),
-('1762649548@qq.com', 4, 'imgStorage/1762649548@qq.com/4/0.png');
 
 -- --------------------------------------------------------
 
@@ -98,13 +59,6 @@ CREATE TABLE `member` (
   `city` varchar(20) NOT NULL,
   `isBanned` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- 转存表中的数据 `member`
---
-
-INSERT INTO `member` (`email`, `name`, `password`, `phone`, `city`, `isBanned`) VALUES
-('1762649548@qq.com', 'bcy', '$2y$10$d4wtWMD031P2iFkZN6p8meFGpAuzZtNz6aLY0ijFxg2kr7dRHnA5u', '6045628672', 'New Westminster', 0);
 
 -- --------------------------------------------------------
 
@@ -139,16 +93,6 @@ CREATE TABLE `post` (
   `value` decimal(20,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- 转存表中的数据 `post`
---
-
-INSERT INTO `post` (`post_id`, `title`, `description`, `date`, `email`, `category`, `isReported`, `isTraded`, `value`) VALUES
-(1, 'Phone', 'isdbabccyigefeong', '2022-12-08 14:22:23', '1762649548@qq.com', 'tv', 0, 0, '100.00'),
-(2, 'sajdbkaa', 'sdcxkn', '2022-12-08 14:23:58', '1762649548@qq.com', 'tv', 0, 0, '100.00'),
-(3, 'asvc', 'asc8xztgivba', '2022-12-08 14:25:38', '1762649548@qq.com', 'tv', 0, 0, '100.00'),
-(4, '123', 'xsxa', '2022-12-08 14:28:36', '1762649548@qq.com', 'figure', 0, 0, '100.00');
-
 -- --------------------------------------------------------
 
 --
@@ -162,37 +106,14 @@ CREATE TABLE `want_to_trade` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 转存表中的数据 `want_to_trade`
---
-
-INSERT INTO `want_to_trade` (`email`, `post_id`, `category`) VALUES
-('1762649548@qq.com', 1, 'tv'),
-('1762649548@qq.com', 2, 'tv'),
-('1762649548@qq.com', 2, 'funko-pop'),
-('1762649548@qq.com', 3, 'tv'),
-('1762649548@qq.com', 4, 'casset');
-
---
 -- 转储表的索引
 --
-
---
--- 表的索引 `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- 表的索引 `collection`
 --
 ALTER TABLE `collection`
   ADD PRIMARY KEY (`collection_id`);
-
---
--- 表的索引 `comment`
---
-ALTER TABLE `comment`
-  ADD PRIMARY KEY (`comment_id`);
 
 --
 -- 表的索引 `image_path`
@@ -225,22 +146,10 @@ ALTER TABLE `post`
 --
 
 --
--- 使用表AUTO_INCREMENT `admin`
---
-ALTER TABLE `admin`
-  MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT;
-
---
 -- 使用表AUTO_INCREMENT `collection`
 --
 ALTER TABLE `collection`
   MODIFY `collection_id` int(20) NOT NULL AUTO_INCREMENT;
-
---
--- 使用表AUTO_INCREMENT `comment`
---
-ALTER TABLE `comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- 使用表AUTO_INCREMENT `message`
@@ -252,7 +161,7 @@ ALTER TABLE `message`
 -- 使用表AUTO_INCREMENT `post`
 --
 ALTER TABLE `post`
-  MODIFY `post_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `post_id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- 限制导出的表
